55cff661dc27ffe7349bb3308e030ff305215f7c
gutierrydsn <gutierrydsn@hotmail.com>
2019-03-24 14:55:32 -0300

    add allowAllCors

--- http-server.js
+++ http-server.js
@@ -1,51 +1,58 @@
 function HttpServer(pPort, pRoute, pPathControllers){
-    
+    const http  = require('http' );    
     const Route = require('./http-route');
 
-    this.start = start;
-    
-    var route = pRoute || require('../../route').route;
-    var pathControllers = pPathControllers || '../../';
+    this.route           = pRoute || require('../../route').route;
+    this.pathControllers = pPathControllers || '../../';
+    this.port            = pPort;
+    this.allowAllCors    = false;
 
-    const http  = require('http' );
-
-    const server = http.createServer(function (req, res){response(req, res)} );
-
-    var port = pPort;
-
-    function response(req, res){        
+    response = (req, res) => {        
         var httpRoute = new Route.HttpRoute();
 
-        httpRoute.route = route; 
-        httpRoute.pathControllers = pathControllers;
-        httpRoute.req = req; 
-        httpRoute.res = res;
+        httpRoute.pathControllers = this.pathControllers;
+        httpRoute.route = this.route; 
+        httpRoute.req   = req; 
+        httpRoute.res   = res;
+       
+        res._headers = {
+            "Content-Type": "application/json; charset=utf-8"
+        };
 
-        console.log(httpRoute);
+        if (this.allowAllCors){
+            res._headers = {
+                "Content-Type"                     : "application/json; charset=utf-8",
+                "Access-Control-Allow-Origin"      : "*",
+                "origin"                           : '*',
+                "Access-Control-Allow-Headers"     : '*',
+                "Access-Control-Expose-Headers"    : '*',
+                "Access-Control-Request-Method"    : '*',
+                "Access-Control-Request-Headers"   : '*',
+                "Access-Control-Allow-Credentials" : 'true',
+                "Access-Control-Allow-Methods"     : '*'
+            }   
+        };
 
-        var head = {
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-        
         try {            
-            var result = JSON.stringify(httpRoute.response());
-            res.writeHead(res.statusCode , head);
-            res.end(result);
+            var result = httpRoute.response();   
+
+            if (result) res.end(JSON.stringify(result));
         }
         catch (e){
-            res.writeHead(500, head);
+            res.statusCode = 500;
             res.end('Error:' + e.message);
             console.log(e);
         }        
     }
 
-    function start(){
-        server.listen(port, '', callBackStartServer());
+    this.start = () => {
+        const server = http.createServer(function (req, res){response(req, res)} );
+        server.listen(this.port, '', callBackStartServer());
     }
 
-    function callBackStartServer(){
-        console.log('Servidor iniciado na porta:' + port)
+    callBackStartServer = () => {
+        console.log('Servidor iniciado na porta:' + this.port)
     }
 }
 
-    module.exports.HttpServer = HttpServer;
+module.exports.HttpServer = HttpServer;
